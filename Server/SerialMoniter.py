8a3ab52b776f988138449fe86a8620b995fd4166
iam7t9 <iam7t9@proton.me>
2023-08-22 16:27:34 +0530

    Pictures uploaded

--- Server/SerialMoniter.py
+++ Server/SerialMoniter.py
@@ -1,5 +1,7 @@
 import serial
 import json
+import serial.tools.list_ports as ports
+
 
 
 def is_json(myjson):
@@ -9,8 +11,19 @@ def is_json(myjson):
     return False
   return True
 
+def getPort():
+    com_ports = list(ports.comports())
+    print("\nAvailable Ports: ")
+    for i in com_ports:
+        print(i.device)
+    if len(com_ports):
+        port = str(com_ports[int(input("Index of selected port: "))].device)
+        return port
+    else:
+        print("Port not detected :(")
+        exit(0)
 
-ser = serial.Serial(port = "COM5", baudrate = 115200)
+ser = serial.Serial(port = getPort(), baudrate = 115200)
 while 1:
     data = ser.readline().decode("ascii")
     if is_json(data):
